34ad627f749cd1e32e35d0f1ebabfbd16bfe495a
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2024-02-07 15:44:22 +0200

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    GET_BY_ID : ``,
-    GET_ALL_SS: '',
-    GET_TRAINERS:''
+    GET_BY_ID : `https://www.adidas.co.uk/api/products`,
+    GET_ALL_SS: 'https://www.adidas.co.uk/api/search/tf/suggestions',
+    GET_TRAINERS:'https://www.adidas.co.uk/api/plp/content-engine?query='
 }
